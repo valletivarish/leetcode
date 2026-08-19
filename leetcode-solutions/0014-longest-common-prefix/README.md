d35cb16c2673887dbb231c7ea9c78a0ad665c4e6
valletivarish <varishvalleti52@gmail.com>
2026-08-20 01:40:02 +0530

    Update Longest Common Prefix solution (Java)

--- leetcode-solutions/0014-longest-common-prefix/README.md
+++ leetcode-solutions/0014-longest-common-prefix/README.md
@@ -13,8 +13,8 @@
 | --- | --- |
 | **Language** | Java |
 | **Runtime** | 1 ms |
-| **Memory** | 43.3 MB |
-| **Submitted** | 2026-08-19 19:51 UTC |
+| **Memory** | 43.2 MB |
+| **Submitted** | 2026-08-19 20:06 UTC |
 | **Solution** | [`Solution.java`](./Solution.java) |
 
 ---
--- leetcode-solutions/0014-longest-common-prefix/Solution.java
+++ leetcode-solutions/0014-longest-common-prefix/Solution.java
@@ -2,6 +2,9 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         StringBuilder sb= new StringBuilder();
         
+
+
+        
         Arrays.sort(strs);
         for(int i=0; i<strs[0].length();i++){
             if(strs[0].charAt(i) != strs[strs.length-1].charAt(i)){
